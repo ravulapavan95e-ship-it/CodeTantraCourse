f04b14e417f126eb735605dc73e31c60416d1793
Lalithasahasra Ryakanti <ravulapavan95e@gmail.com>
2026-02-14 16:42:52 +0530

    Create StructureOfJavaProgram.java

---
+++ StructureOfJavaProgram.java
@@ -0,0 +1,9 @@
+package q10734;
+public class FirstJavaProgram{
+public static void main(String[] args){
+System.out.println("India got its independence in 1947");
+}
+}
+//Within the class, define the main method.
+/*In the main method Write the print statement
+in order to print the text "India got its independence in 1947"*/
