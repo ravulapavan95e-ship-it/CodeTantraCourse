4d3fc0ad47ab02d1f7e8c832abee96eb8f0848c3
Lalithasahasra Ryakanti <ravulapavan95e@gmail.com>
2026-02-16 20:39:34 +0530

    Create FloatDemo.java

---
+++ FloatDemo.java
@@ -0,0 +1,9 @@
+package q10858;
+public class FloatDemo {
+public static void main(String[] args) {
+float x = 1.5f;
+double y = 1.1d;
+float sum =(float) (x + y);
+System.out.println("sum = " + sum);// should print 2.6
+}
+}
