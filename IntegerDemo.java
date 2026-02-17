c00d9fafc6e4947684dea7964ddb160716a51681
Lalithasahasra Ryakanti <ravulapavan95e@gmail.com>
2026-02-17 23:34:26 +0530

    Create IntegerDemo.java

---
+++ IntegerDemo.java
@@ -0,0 +1,21 @@
+package q10770;
+public class IntegerDemo {
+public static void main(String args[]) {
+int decimalOne = 1;
+System.out.println("decimalOne = " +
+decimalOne);
+int binaryThree = 0b11;
+System.out.println("binaryThree = " +
+binaryThree);
+int octalEight = 010;
+System.out.println("octalEight = " +
+octalEight);
+int hexTen = 0xA;
+System.out.println("hexTen = " + hexTen);
+char unicodeValueOfA = 'A';
+System.out.println("unicodeValue Of A = " + 65);
+char unicodeValueOfZ = 'Z';
+System.out.println("unicodeValue Of Z =
+" + 90);
+}
+}
