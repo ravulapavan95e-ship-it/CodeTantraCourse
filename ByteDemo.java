4ad4a71fedec9c4a222114ca6238c5e4e53fc1cd
Lalithasahasra Ryakanti <ravulapavan95e@gmail.com>
2026-02-17 23:39:16 +0530

    Create ByteDemo.java

---
+++ ByteDemo.java
@@ -0,0 +1,10 @@
+package q10778;
+public class ByteDemo {
+public static void main(String[] args) {
+byte b1 = 4;
+byte b2 = 20;
+byte sum =(byte) (b1 + b2);
+System.out.println("sum = " + sum);//
+should print 24
+}
+}
