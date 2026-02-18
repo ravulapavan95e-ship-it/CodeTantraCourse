ef8b1e39a675395e8c255c12246cccd5edd4a16f
Lalithasahasra Ryakanti <ravulapavan95e@gmail.com>
2026-02-18 22:29:40 +0530

    update Types

---
+++ PrimitiveTypes.java
@@ -0,0 +1,18 @@
+class PrimitiveTypes {
+static byte m;
+static short l;
+static int s;
+static long a;
+static boolean b;
+static double d;
+static float f;
+public static void main(String[] args) {
+System.out.println("byte default value = " + m);
+System.out.println("short default value = " + l);
+System.out.println("int default value =  " + s);
+System.out.println("long default value = " + a);
+System.out.println("boolean defaultvalue = " + b);
+System.out.println("double default value = " + d);
+System.out.println("float default value  = " + f);
+}
+}
